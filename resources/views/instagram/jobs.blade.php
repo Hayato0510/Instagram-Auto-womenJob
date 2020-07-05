7846e3f4c9dfb6223e5b099f2b8af0a80bdc1321
EC2 Default User <ec2-user@ip-172-31-22-63.us-east-2.compute.internal>
2020-07-05 01:45:59 +0000

    mm

--- resources/views/instagram/jobs.blade.php
+++ resources/views/instagram/jobs.blade.php
@@ -6,13 +6,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>公式ページへ移動中</title>
         <link href="{{ secure_asset('css/jobs.css') }}" rel="stylesheet">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
         <!--自動遷移設定-->
         <!--<meta http-equiv="refresh" content=" 5; url=./os">-->
     </head>
     <body>
+        <img src="{{ asset('image/womenJob_top_small.jpg') }}"></img>
         <br>
-        <br>
-        <p>公式ページへ移動します。</p>
-        <p><a href="./os" class="text">コチラ</a>をタップし、公式ページをご確認下さい。</p>
+        <a href="./os" class="btn-square-so-pop"><i class="fab fa-apple"></i>/<i class="fab fa-android"></i> 公式ページへ <i class="fas fa-arrow-circle-right"></i></a>
+        <p>※期間限定募集の為、リンク切れの場合は募集停止しています。ご了承下さい。</p>
     </body>
 </html>
